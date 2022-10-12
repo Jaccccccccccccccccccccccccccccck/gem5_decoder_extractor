096f2f45e16b1940e8a06b0cc8b08d010d8a519f
root <1163949417@qq.com>
2022-10-12 10:27:46 +0800

    add some test case

--- main.cc
+++ main.cc
@@ -5,12 +5,29 @@
 using namespace std;
 
 int main(int argc,char *argv[]) {
-    unsigned char bytes[4];  // 48 83 ec 08
-    bytes[0] = 0x48;
-    bytes[1] = 0x83;
-    bytes[2] = 0xec;
-    bytes[3] = 0x08;
-    gem5::X86ISA::ExtMachInst extInst = gem5::X86ISA::Decoder::getExtInst(bytes, 4);
+    unsigned char bytes1[6];  // ff 25 aa 0a 20 00 : jmpq   *0x200aaa(%rip)        # 200fd0 <printf@GLIBC_2.2.5>
+    bytes1[0] = 0xff;
+    bytes1[1] = 0x25;
+    bytes1[2] = 0xaa;
+    bytes1[3] = 0x0a;
+    bytes1[4] = 0x20;
+    bytes1[5] = 0x00;
+    gem5::X86ISA::ExtMachInst extInst = gem5::X86ISA::Decoder::getExtInst(bytes1, sizeof(bytes1));
     gem5::StaticInst *a = gem5::X86ISA::Decoder::decodeInst(extInst).get();
     cout << a->getName() << endl;
+    unsigned char bytes2[4];  // 48 83 ec 08  sub    $0x8,%rsp
+    bytes2[0] = 0x48;
+    bytes2[1] = 0x83;
+    bytes2[2] = 0xec;
+    bytes2[3] = 0x08;
+    gem5::X86ISA::ExtMachInst extInst2 = gem5::X86ISA::Decoder::getExtInst(bytes2, sizeof(bytes2));
+    gem5::StaticInst *b = gem5::X86ISA::Decoder::decodeInst(extInst2).get();
+    cout << b->getName() << endl;
+
+    unsigned char bytes3[2];  // 48 83 ec 08  sub    $0x8,%rsp
+    bytes2[0] = 0x66;
+    bytes2[1] = 0x90;
+    gem5::X86ISA::ExtMachInst extInst3 = gem5::X86ISA::Decoder::getExtInst(bytes2, sizeof(bytes2));
+    gem5::StaticInst *c = gem5::X86ISA::Decoder::decodeInst(extInst3).get();
+    cout << c->getName() << endl;
 }
